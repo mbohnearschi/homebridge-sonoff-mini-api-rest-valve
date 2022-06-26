8944c628144b9acb06e58a7926f290801a0a6132
Mihail Bohnearschi <mihail.bohnearschi@endava.com>
2022-06-26 12:55:06 +0300

    Fixed state parsing

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ SonoffAccessory.prototype.getStateBool = function(callback) {
   .end((error, response) => {
     if (!error && response.statusCode == 200) {
       var json = JSON.parse(response.text).data;
-      var state = JSON.parse(json).switch;
+      var state = json.switch;
 
       if (this.debug)           
           this.log('getState() request returned successfully ('+response.statusCode+'). Body: '+JSON.stringify(json));
